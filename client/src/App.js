08a2ae192125e3f640ecd84923f31930829b654e
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-12-15 01:53:56 +0530

    header designed

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Dashboard from "./pages/dashboard";
 import Employees from "./pages/employees";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
       <div className="App">
         <Header />
-        <Switch>
+        <Routes>
           <Route path="/" exact element={<Dashboard />} />
           <Route path="/employees" exact element={<Employees />} />
-        </Switch>
+        </Routes>
         <Footer />
       </div>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+// Header.js
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <div>
-        Header
-    </div>
-  )
-}
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+      <Navbar.Brand href="#home">Your Logo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#services">Services</Nav.Link>
+        </Nav>
+        <Nav>
+          <NavDropdown title="Profile" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#profile">My Profile</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#notifications">Notifications</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
